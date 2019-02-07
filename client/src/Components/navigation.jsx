7014a61ec25a0a0744b2ca630a43d2ad48da7203
JeongJoonChoe <peter_choe@LAPTOP-5JA8GFEA.localdomain>
2019-02-06 20:28:19 -0800

    nav changes

---
+++ client/src/Components/navigation.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class NavBar extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <ol>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ol>
+      </div>
+    )
+  }
+}
