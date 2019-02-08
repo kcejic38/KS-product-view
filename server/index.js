8d1c6022e15d06711086ca4fc52c16e44c5161b6
JeongJoonChoe <peter_choe@LAPTOP-5JA8GFEA.localdomain>
2019-02-08 13:46:21 -0800

    basic get requests for product

--- server/index.js
+++ server/index.js
@@ -1,10 +1,31 @@
 const express = require('express');
+const db = require('../database/index.js');
 
 const app = express();
 const port = 8002;
 
 app.use(express.static(__dirname + '/../public'));
 app.use(express.json());
+
+app.get('/products', (req, res) => {
+  let model = req.body.model;
+
+  db.findall({
+    where: {
+      model: model
+    }
+  })
+  .then((data) => {
+    res.json(data);
+  })
+})
+
+app.get('/images', (req, res) => {
+  let imageID = req.body.imageID;
+
+})
+
+
 
 app.listen(port, ()=>{
   console.log(`listening on port ${port}`);
