025547e92ac131e6dd48676dff3018078381794d
JeongJoonChoe <peter_choe@LAPTOP-5JA8GFEA.localdomain>
2019-02-12 23:33:42 -0800

    checkout buttons styling complete

--- client/src/Components/Product-info/info.jsx
+++ client/src/Components/Product-info/info.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ProductInfo from './model-info.jsx';
 import RelatedProducts from './related-products.jsx';
+import Checkout from './checkout.jsx';
+
 
 class InfoSection extends React.Component {
   render() {
@@ -8,6 +10,7 @@ class InfoSection extends React.Component {
       <div className="info-container">
         <ProductInfo />
         <RelatedProducts />
+        <Checkout />
       </div>
     )
   }
