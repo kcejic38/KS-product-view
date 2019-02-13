b47d059cdce96a2cf6371e162cbde9c467d13b21
JeongJoonChoe <peter_choe@LAPTOP-5JA8GFEA.localdomain>
2019-02-12 18:05:30 -0800

    available colors layout

--- client/src/Components/Product-info/info.jsx
+++ client/src/Components/Product-info/info.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ProductInfo from './model-info.jsx';
+import RelatedProducts from './related-products.jsx';
 
 class InfoSection extends React.Component {
   render() {
     return (
       <div className="info-container">
         <ProductInfo />
+        <RelatedProducts />
       </div>
     )
   }
---
+++ client/src/Components/Product-info/related-products.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const RelatedProducts = (props) => {
+  return (
+    <div className="related-container">
+      <h5>Available Colors</h5>
+      <ol>
+        <li>TODO1</li> /
+        <li>TODO2</li> /
+        <li>TODO3</li>
+      </ol>
+    </div>
+  )
+}
+
+export default RelatedProducts;
