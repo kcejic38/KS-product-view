23f76fb931472a0154bba06cb86addb154234803
JeongJoonChoe <peter_choe@LAPTOP-5JA8GFEA.localdomain>
2019-02-12 20:40:24 -0800

    other available colors component and styling

--- client/src/Components/Product-info/related-products.jsx
+++ client/src/Components/Product-info/related-products.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Image from 'react-bootstrap/Image';
+
 
 const RelatedProducts = (props) => {
   return (
@@ -9,6 +11,11 @@ const RelatedProducts = (props) => {
         <li>TODO2</li> /
         <li>TODO3</li>
       </ol>
+      <div className="other-colors">
+        <Image className="other-thumbnail" src="https://assets.adidas.com/images/w_840,h_840,f_auto,q_auto:sensitive,fl_lossy/9bd9d0ae47aa4e6c85b8a97e012f405b_9366/Ultraboost_All_Terrain_Shoes_Grey_F35236_01_standard.jpg" roundedCircle />
+        <Image className="other-thumbnail" src="https://assets.adidas.com/images/w_840,h_840,f_auto,q_auto:sensitive,fl_lossy/9bd9d0ae47aa4e6c85b8a97e012f405b_9366/Ultraboost_All_Terrain_Shoes_Grey_F35236_01_standard.jpg" roundedCircle />
+        <Image className="other-thumbnail" src="https://assets.adidas.com/images/w_840,h_840,f_auto,q_auto:sensitive,fl_lossy/9bd9d0ae47aa4e6c85b8a97e012f405b_9366/Ultraboost_All_Terrain_Shoes_Grey_F35236_01_standard.jpg" roundedCircle />
+      </div>
     </div>
   )
 }
