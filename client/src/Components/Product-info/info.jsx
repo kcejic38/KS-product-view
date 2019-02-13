f3dad577dc65acf15580494bfb59dbb0e04c635a
JeongJoonChoe <peter_choe@LAPTOP-5JA8GFEA.localdomain>
2019-02-12 17:48:13 -0800

    model title and review component with styling

--- client/src/Components/Product-info/info.jsx
+++ client/src/Components/Product-info/info.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import ProductInfo from './model-info.jsx';
 
 class InfoSection extends React.Component {
   render() {
     return (
       <div className="info-container">
-        <h1> info</h1>
+        <ProductInfo />
       </div>
     )
   }
