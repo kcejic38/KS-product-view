e6a9a27b0a51e497416c0ead186017b5c08c81f7
Katarina Svorcan <kata_che@hotmail.com>
2019-02-27 22:31:53 -0800

    Run the app, seed db, and update configs

--- database/index.js
+++ database/index.js
@@ -1,32 +1,33 @@
 const Sequelize = require('sequelize');
 const config = require('../config.js');
 
-const db = new Sequelize('Products', 'root', config.pw, {
+const db = new Sequelize('Products', config.user, config.pw, {
   host: 'localhost',
   dialect: 'mysql'
-  }
+}
 )
 
 const Images = db.define('images', {
-  img_id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
-  links: {type: Sequelize.TEXT}
+  img_id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+  links: { type: Sequelize.TEXT }
 })
 
 const Shoes = db.define('shoes', {
-  id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
-  colors: { type: Sequelize.STRING, allowNull: false},
-  type: { type: Sequelize.STRING, allowNull: false},
-  model: { type: Sequelize.STRING, allowNull: false},
-  sizes: { type: Sequelize.STRING, allowNull: false},
-  price: { type: Sequelize.INTEGER, allowNull: false},
-  image_ID: { type: Sequelize.INTEGER,
+  id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+  colors: { type: Sequelize.STRING, allowNull: false },
+  type: { type: Sequelize.STRING, allowNull: false },
+  model: { type: Sequelize.STRING, allowNull: false },
+  sizes: { type: Sequelize.STRING, allowNull: false },
+  price: { type: Sequelize.INTEGER, allowNull: false },
+  image_ID: {
+    type: Sequelize.INTEGER,
     references: {
-        model: Images,
-        key: 'img_id'
-      }
+      model: Images,
+      key: 'img_id'
+    }
   },
-  review_count: { type: Sequelize.INTEGER, allowNull: false},
-  avg_stars: { type: Sequelize.INTEGER, allowNull: false}
+  review_count: { type: Sequelize.INTEGER, allowNull: false },
+  avg_stars: { type: Sequelize.INTEGER, allowNull: false }
 })
 
 const DB = {
